b8d393fe31169c4522648e62a3c24f1448d9cd31
GabrielDSanta <eloviveiros@hotmail.com>
2022-11-09 23:01:24 -0300

    Exercicio Finalizado

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
 
-const Numero = 5
+// const Numero = 5
 
-console.log(Numero % 2 == 0 ? "É primo" : "Não é primo")
+// console.log(Numero % 2 == 0 ? "É primo" : "Não é primo")
+
+
+let hora = 1
+let minutos = 0
+
+hora > 0 ? minutos = hora * 60 : minutos = 60
