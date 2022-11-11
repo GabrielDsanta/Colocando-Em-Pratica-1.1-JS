444ef4500780e943e02d88bc0523a19bc21a9ba4
GabrielDSanta <eloviveiros@hotmail.com>
2022-11-10 22:12:35 -0300

    Exercicio Finalizado

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
 
-// Exercicio 01//
+let Username
+let Password
 
-const ListaNumeros = [12,6,9,42,11,102,44,15]
-
-let Resultado = 0
-
-// for(let index = 0; index < ListaNumeros.length; index++) {
-//      Resultado += ListaNumeros[index]
-// }
-
-// console.log(Resultado)
-
-// let index = 0
-
-// while(index <= 7){
-//     Resultado += ListaNumeros[index]
-//     index++
-// }
-
-// console.log(Resultado)
-
-// let index = 0
-// do{
-//     Resultado += ListaNumeros[index]
-//     index++
-// }while(index <= 7)
-
-// console.log(Resultado)
+RequestLogin(Username, Password)
 
 
+function RequestRegister(){
+    Username = prompt("Digite seu Usuário")
+    Password = prompt("Digite sua Senha")
+}
 
+function RequestLogin(name, password){
+    name = prompt("Digite seu Usuário")
+    password = prompt("Digite sua Senha")
 
-// Exercicio 02//
+    name === Username && password === Password ? true : false
+}
 
-// for (let index = 0; index < ListaNumeros.length; index++){
-//     ListaNumeros[index] % 1 === 0 && ListaNumeros[index] % ListaNumeros[index] === 0 && ListaNumeros[index] % 2 !== 0 && ListaNumeros[index] % 3 !== 0  ? index = 10 : console.log(ListaNumeros[index])
-// }
-
-// for (let index = 0; index < ListaNumeros.length; index++) {
-//     ListaNumeros[index] < 40 ? console.log(ListaNumeros[index]) : ListaNumeros[index] > 50 && (console.log(ListaNumeros[index]))
-// }
