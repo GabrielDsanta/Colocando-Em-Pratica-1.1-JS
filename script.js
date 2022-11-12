29e83f130b9b834f391702ef516495e0b3f0f32a
GabrielDSanta <eloviveiros@hotmail.com>
2022-11-11 22:52:03 -0300

    Exercicio Finalizado

--- script.js
+++ script.js
@@ -1,96 +1,18 @@
 
-//Exercicio 01//
 
-// var idade = 20
-// function getAnoNascimento(){
-//     var ano = 2022 - idade
-//     return ano
-// }
+function CalcularSalario(salarioAtual){
+    typeof salarioAtual === "string" && console.error("Não é possível calcular um texto, por favor insira um número")
+    try{
+        let salarioFinal
+        salarioAtual <= 1500 && (salarioFinal = salarioAtual * 1.20)
+        salarioAtual > 1500 && salarioAtual <= 2000  && (salarioFinal = salarioAtual * 1.15)
+        salarioAtual > 2000 && salarioAtual <= 3000  && (salarioFinal = salarioAtual * 1.10)
+        salarioAtual > 3000 && (salarioFinal = salarioAtual * 1.05)
 
-// function confirmarDados (nome){
-//     var nomeCompleto = nome + 'da Silva'
-//     console.log ("nome completo", nomeCompleto, "ano de nascimento", getAnoNascimento(), "idade", idade)
-// }
-// confirmarDados ("Marcos")
+        typeof salarioAtual !== 'string' && (console.log(salarioFinal))
+    }
+    catch(error){
+    }
+}
 
-
-
-
-
-
-//Exercicio 02//
-
-// let idade = 40
-// function ConfirmarDados(nome){
-//     const nomeCompleto = nome = 'Da Silva'
-//     let geracao = "millenium"
-
-//     function getAnoNascimento(){
-//         const idade = 40
-//         const ano = 2022 - idade
-//         return ano
-//     }
-
-//     let anoDeNascimento = getAnoNascimento()
-//     if(anoDeNascimento <= 1982){
-//         geracao = "Geração X"
-//     }
-//     console.log ("nome completo", nomeCompleto, "ano de nascimento", getAnoNascimento(), "idade", idade)
-// }
-
-// ConfirmarDados("Marcos")
-
-
-
-
-
-
-//Exercicio 03//
-
-// let idade = 20
-
-// function confirmarDados(nome) {
-//     try {
-//       let nomeInteiro = nome + "da Silva"
-
-//       function getAnoNascimento() {
-//         let ano = 2022 - idade;
-//         return ano;
-//       }
-//       return console.log ("nome completo", nomeInteiro, "ano de nascimento", getAnoNascimento(), "idade", idade)
-//     } 
-
-//     catch (error) {
-//       console.log("um erro foi encontrado e não foi possível concluir a operação ", error);
-//     }
-
-//   }
-
-// confirmarDados()
-
-
-
-//Exercicio 04//
-
-
-// let idade = 40
-
-// function ConfirmarDados(nome){
-//     const nomeCompleto = nome = 'Da Silva'
-
-//     function getAnoNascimento(){
-//         const ano = 2022 - idade
-//         return ano
-//     }
-
-//     function geracaoPertence(){
-//         let geracao
-//         getAnoNascimento() <= 1982 ? geracao = "Geração X" : geracao = "millenium"
-//         return geracao
-//     }
-
-//     let anoDeNascimento = getAnoNascimento()
-//     console.log ("nome completo", nomeCompleto, "ano de nascimento", anoDeNascimento, "idade", idade, "Geração", geracaoPertence())
-// }
-
-// ConfirmarDados("Marcos")
+CalcularSalario("ASdaSDASD")
