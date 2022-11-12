a05cdfc17277b0bdb5b0c421b9572888b1e29ac3
GabrielDSanta <eloviveiros@hotmail.com>
2022-11-11 21:52:59 -0300

    Todos os Exercicios finalizados

--- script.js
+++ script.js
@@ -1,19 +1,96 @@
 
-let Username
-let Password
+//Exercicio 01//
 
-RequestLogin(Username, Password)
+// var idade = 20
+// function getAnoNascimento(){
+//     var ano = 2022 - idade
+//     return ano
+// }
+
+// function confirmarDados (nome){
+//     var nomeCompleto = nome + 'da Silva'
+//     console.log ("nome completo", nomeCompleto, "ano de nascimento", getAnoNascimento(), "idade", idade)
+// }
+// confirmarDados ("Marcos")
 
 
-function RequestRegister(){
-    Username = prompt("Digite seu Usuário")
-    Password = prompt("Digite sua Senha")
-}
 
-function RequestLogin(name, password){
-    name = prompt("Digite seu Usuário")
-    password = prompt("Digite sua Senha")
 
-    name === Username && password === Password ? true : false
-}
 
+
+//Exercicio 02//
+
+// let idade = 40
+// function ConfirmarDados(nome){
+//     const nomeCompleto = nome = 'Da Silva'
+//     let geracao = "millenium"
+
+//     function getAnoNascimento(){
+//         const idade = 40
+//         const ano = 2022 - idade
+//         return ano
+//     }
+
+//     let anoDeNascimento = getAnoNascimento()
+//     if(anoDeNascimento <= 1982){
+//         geracao = "Geração X"
+//     }
+//     console.log ("nome completo", nomeCompleto, "ano de nascimento", getAnoNascimento(), "idade", idade)
+// }
+
+// ConfirmarDados("Marcos")
+
+
+
+
+
+
+//Exercicio 03//
+
+// let idade = 20
+
+// function confirmarDados(nome) {
+//     try {
+//       let nomeInteiro = nome + "da Silva"
+
+//       function getAnoNascimento() {
+//         let ano = 2022 - idade;
+//         return ano;
+//       }
+//       return console.log ("nome completo", nomeInteiro, "ano de nascimento", getAnoNascimento(), "idade", idade)
+//     } 
+
+//     catch (error) {
+//       console.log("um erro foi encontrado e não foi possível concluir a operação ", error);
+//     }
+
+//   }
+
+// confirmarDados()
+
+
+
+//Exercicio 04//
+
+
+// let idade = 40
+
+// function ConfirmarDados(nome){
+//     const nomeCompleto = nome = 'Da Silva'
+
+//     function getAnoNascimento(){
+//         const ano = 2022 - idade
+//         return ano
+//     }
+
+//     function geracaoPertence(){
+//         let geracao
+//         getAnoNascimento() <= 1982 ? geracao = "Geração X" : geracao = "millenium"
+//         return geracao
+//     }
+
+//     let anoDeNascimento = getAnoNascimento()
+//     console.log ("nome completo", nomeCompleto, "ano de nascimento", anoDeNascimento, "idade", idade, "Geração", geracaoPertence())
+// }
+
+// ConfirmarDados("Marcos")
